1097aedcee65a7984bd3fc0c991f25aecf51cbd8
gerimori8 <gerardmp2008@gmail.com>
2026-02-20 17:37:47 +0100

    1.1.5

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion, useScroll, useSpring } from 'framer-motion';
 import { LanguageProvider } from './context/LanguageContext';
 import Header from './components/Header';
